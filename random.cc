0c906e9f656783c2566c46607135c41f2d51a805
liubin8 <liubin8@localhost.localdomain>
2015-10-22 21:41:46 +0800

    random unsigned int

--- random.cc
+++ random.cc
@@ -8,7 +8,8 @@
 #include <cstdlib>  // srand, rand
 using namespace std;  
 
-#define MAX_BIT 23
+#define MAX_BIT  23
+#define FILENAME "out.txt"
 
 /* *
  * View http://www.cnblogs.com/zhenjing/archive/2012/06/10/c_random.html
@@ -16,21 +17,30 @@ using namespace std;
  * 2) srand/srandom性能极差极差。只有调用rand的1%，没必要每次调用rand之前都调用srand。
  *
  * int rand(void);        return [0, 1 << 31) 
- * long int random(void); return [0, 1 << 63) (64bit system)
+ * long int random(void); return [0, 1 << 63) (64bit system) ?? TODO
  *
  * (((1<<31) - 1000000000) * 10 + 900000000 * 9 + 90000000 * 8 + 9000000 * 7 + 900000 * 6 
  *  + 90000 * 5 + 9000 * 4 + 900 * 3 + 90 * 2 + 10)  * 1.0/ (1<<31) ==  9.482598570175469
+ *  9.741299285087734 for unsigned int
  * */
-int random(ofstream &out, int n) {
+void randomToFile(ofstream &out, int n) {
+    unsigned int t;
     int i = 0;
     time_t begin, end;
 
+    // srandom(time(NULL));
     srand(time(NULL));
+    cout << sizeof(long int) << " " << sizeof(1L) << endl;
 
     begin = time(NULL);
     // ~= 1M/s
     while(i++ < n) {
-        out << rand() << endl;
+        //out << (random() & 0xFFFFFFFFL) << endl; // not work
+        t = rand();
+        if(rand() & 1) {
+            t |= 0x80000000;
+        }
+        out << t << endl;
     }
     end = time(NULL);
 
@@ -41,12 +51,12 @@ int main() {
     /* *
      * CPP File: http://blog.csdn.net/lh3325251325/article/details/4761575
      * */
-    ofstream out("out.txt");  
+    ofstream out(FILENAME);  
     if(!out){  
         cout << "Unable to open outfile" << endl;  
         return -1;
     }  
-    random(out, 1 << MAX_BIT);
+    randomToFile(out, 1 << MAX_BIT);
 
     out.close();  
     return 0;  
